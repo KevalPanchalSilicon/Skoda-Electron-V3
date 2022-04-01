4c35e398f770a4928555bcf55ba46ea9a9a3033b
Keval Panchal <kevalpanchal@siliconithub.com>
2022-04-01 10:37:36 +0530

    Update with 1.2.9

--- src/page/Login.js
+++ src/page/Login.js
@@ -88,9 +88,9 @@ const Login = observer((props) => {
 	};
 
 	useEffect(() => {
-		console.log("object : ","version latest 1.2.8");
+		console.log("object : ","version latest 1.2.9");
 		const timer = setInterval(() => {
-			console.log("object : ","1.2.8",localStorage.getItem("UUID"));
+			console.log("object : ","1.2.9",localStorage.getItem("UUID"));
 		}, 1000);
 		if (remember_me) {
 			form.setFieldsValue({
@@ -138,7 +138,7 @@ const Login = observer((props) => {
 						<img src={company ? company.branding.logo : logo} alt="Logo" />
 					</div>
 					<Typography.Title level={2} className="w-100 login_title">
-						Login 1.2.8
+						Login 1.2.9
 					</Typography.Title>
 					{hasCamera && cameraPermission !== "denied" && 
 						<>
